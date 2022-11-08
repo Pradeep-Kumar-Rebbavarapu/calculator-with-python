49a8ede5aa2c5e1bcd992b1f13f37a9fe3303530
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2022-11-08 14:53:25 +0530

    i

--- tut1.py
+++ tut1.py
@@ -18,7 +18,7 @@ def sinx(x):
 def cosx(x):
     sign = 1
     total_sum = 0
-    if x!=0 and x%(PI/2)==0:
+    if x!=0 and x%(PI/2)==0 and x%(PI)!=0:
         return 0
     for i in range(0,31,2):
         cosx = ((sign) * (x**i))/factorial(i)
@@ -29,52 +29,72 @@ tanx = lambda x:'not defined' if cosx(x)==0 else sinx(x)/cosx(x)
 cotx = lambda x:'not defined' if sinx(x)==0 else cosx(x)/sinx(x)
 secx = lambda x:'not defined' if cosx(x)==0 else 1/cosx(x)
 cosecx = lambda x:'not defined' if sinx(x)==0 else 1/sinx(x)
-operators = ['+','-','*','/','^']
+operators = ['-','+','*','/','^']
 while True:
-    ch = input('===>')  
-    string_with_no_white_spaces = ch.replace(' ','').lower()
-    if '(' and ')' in string_with_no_white_spaces:
-        angle_value_in_radian = string_with_no_white_spaces.split('(')[1].replace(")","")
-        trig_string = string_with_no_white_spaces.replace("(","").replace(angle_value_in_radian,'').replace(")","")
-        
-        trig_list = [trig_string,angle_value_in_radian]
+    try:
+        ch = input('===>')  
+        string_with_no_white_spaces = ch.replace(' ','').lower()
+        if '(' and ')' in string_with_no_white_spaces:
+            angle_value_in_radian = string_with_no_white_spaces.split('(')[1].replace(")","")
+            trig_string = string_with_no_white_spaces.replace("(","").replace(angle_value_in_radian,'').replace(")","")
+            angle_string_list = []
             
+            if 'pi' in angle_value_in_radian:
+                if '/' in angle_value_in_radian:
+
+                    trig_list = [trig_string,PI/int(angle_value_in_radian.split('/')[1])]
+                else:
+                    print(angle_value_in_radian.split('pi'))
+                    if len(angle_value_in_radian.split('pi')[0])==0:
+                        trig_list = [trig_string,PI]
+                    else:
+                        trig_list = [trig_string,int(angle_value_in_radian.aplit('pi')[0]) * PI]
+            else:
+                trig_list = [trig_string,angle_value_in_radian]
+                
+            
+            
+            if trig_list[0]=="sin":
+                print(sinx(float(trig_list[1])))
+            if trig_list[0]=="cos":
+                print(cosx(float(trig_list[1])))
+            if trig_list[0]=="tan":
+                print(tanx(float(trig_list[1])))
+            if trig_list[0]=="cot":
+                print(cotx(float(trig_list[1])))
+            if trig_list[0]=="cosec":
+                print(cosecx(float(trig_list[1])))
+            if trig_list[0]=="sec":
+                print(secx(float(trig_list[1])))
+            print(trig_list)
+        elif '(' and ')' not in string_with_no_white_spaces:
+            string_list_with_operator = [i for i in string_with_no_white_spaces]
+            for i in operators:
+                if i in string_with_no_white_spaces:
+                    s = string_with_no_white_spaces.replace(i,'')
+                    string_list_without_operator = string_with_no_white_spaces.split(i)
+                    print(s)
+                    print(string_list_without_operator)
+            for i in s:
+                only_operator_list = string_list_with_operator.remove(i)
+            print('only operator list',only_operator_list)
         
-        
-        if trig_list[0]=="sin":
-            print(sinx(float(trig_list[1])))
-        if trig_list[0]=="cos":
-            print(cosx(float(trig_list[1])))
-        if trig_list[0]=="tan":
-            print(tanx(float(trig_list[1])))
-        if trig_list[0]=="cot":
-            print(cotx(float(trig_list[1])))
-        if trig_list[0]=="cosec":
-            print(cosecx(float(trig_list[1])))
-        if trig_list[0]=="sec":
-            print(secx(float(trig_list[1])))
-    elif '(' and ')' not in string_with_no_white_spaces:
-        string_list_with_operator = [i for i in string_with_no_white_spaces]
-        for i in operators:
-            if i in string_with_no_white_spaces:
-                s = string_with_no_white_spaces.replace(i,'')
-                string_list_without_operator = string_with_no_white_spaces.split(i)
-        for i in s:
-            only_operator_list = string_list_with_operator.remove(i)
-    
-        string_list = string_list_without_operator + string_list_with_operator
-        if string_list[2]=="+":
-            print(Addition(float(string_list[0]),float(string_list[1])))
-        if string_list[2]=="-":
-            print(Subtraction(float(string_list[0]),float(string_list[1])))
-        if string_list[2]=="*":
-            print(Multiplication(float(string_list[0]),float(string_list[1])))
-        if string_list[2]=="/":
-            print(Division(float(string_list[0]),float(string_list[1])))
-        if string_list[2]=="^":
-            print(Exponential(float(string_list[0]),float(string_list[1])))
-        if string_list[2]=="!":
-            print(factorial(float(string_list[0])))    
+            string_list = string_list_without_operator + string_list_with_operator
+            print('string_list',string_list)
+            if string_list[2]=="+":
+                print(Addition(float(string_list[0]),float(string_list[1])))
+            if string_list[2]=="-":
+                print(Subtraction(float(string_list[0]),float(string_list[1])))
+            if string_list[2]=="*":
+                print(Multiplication(float(string_list[0]),float(string_list[1])))
+            if string_list[2]=="/":
+                print(Division(float(string_list[0]),float(string_list[1])))
+            if string_list[2]=="^":
+                print(Exponential(float(string_list[0]),float(string_list[1])))
+            if string_list[2]=="!":
+                print(factorial(float(string_list[0]))) 
+    except Exception as e:
+        print('Please Enter Valid Values')   
     
     
     
